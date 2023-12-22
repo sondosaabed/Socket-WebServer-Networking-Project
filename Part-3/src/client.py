10fd4e43cad1c40d0cb02e17b3fed13f90d4dae7
Sondos <sondosahmedabed@gmail.com>
2023-12-22 21:33:23 +0200

    Delete client Part 3

--- Part-3/src/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def run_client(server_ip, server_port):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((server_ip, server_port))
-    
-    client.close()
-
-run_client("127.0.0.1", 8000)
